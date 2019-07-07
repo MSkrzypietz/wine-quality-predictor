04dfd5d2faa8356adf47eb92dbe7e13bcf12cd02
Michael Skrzypietz <michael.skr97@gmail.com>
2019-07-07 11:46:51 +0200

    added preprocessing scaler and cross validation pipeline

--- wine_quality_predictor.py
+++ wine_quality_predictor.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing   
+from sklearn import preprocessing
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import make_pipeline
@@ -17,3 +17,11 @@ data = pd.read_csv(dataset_url, sep=';')
 y = data.quality
 X = data.drop('quality', axis=1)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=123,
+                                                    stratify=y)
+
+scaler = preprocessing.StandardScaler().fit(X_train)
+pipeline = make_pipeline(preprocessing.StandardScaler(),
+                         RandomForestRegressor(n_estimators=100))
